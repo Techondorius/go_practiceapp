41ee6d140f01055db94db7fa9134f49ab3f739e3
Techondorius <kurofuku01@gmail.com>
2022-06-23 12:15:00 +0900

    db接続

--- main.go
+++ main.go
@@ -4,9 +4,26 @@ import (
 	// "fmt"
 	"github.com/gin-gonic/gin"
 	"go_practiceapp/routers"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+type Product struct {
+    gorm.Model
+    Code  string
+    Price uint
+}
+
 func main(){
+
+	db, err := gorm.Open("mysql", "root:@tcp(db:3306)/gin_app?charset=utf8&parseTime=True&loc=Local")
+    if err != nil {
+        panic("failed to connect database")
+    }
+    defer db.Close()
+
+    db.AutoMigrate(&Product{})
+
 	r := gin.Default()
 
 	userManage := r.Group("/userManage")
