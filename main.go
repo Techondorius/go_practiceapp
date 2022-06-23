7dd0fbcb6c17cb6db12f8efb5bb447403c01a732
Techondorius <kurofuku01@gmail.com>
2022-06-23 11:30:56 +0900

    雛形作成

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	// "fmt"
+	"github.com/gin-gonic/gin"
+	"go_practiceapp/routers"
+)
+
+func main(){
+	r := gin.Default()
+
+	userManage := r.Group("/userManage")
+	{
+		userManage.POST("/newUser", routers.NewUser)
+		userManage.PUT("/editUser/:userId", routers.EditUser)
+		userManage.DELETE("/deleteUser/:userId", routers.DeleteUser)
+	}
+
+	stamp := r.Group("/stamp/:userId")
+	{
+		stamp.POST("/in", routers.User_in)
+		stamp.POST("/up", routers.User_in)
+	}
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{ "message": "good", })
+	})
+
+	r.Run()
+}
